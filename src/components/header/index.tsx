0af260fa874ab4e00ec18822bcea3e870e4cbc5f
Jose Luis Rodriguez Peñaranda <soltec_cucuta@hotmail.com>
2021-09-23 10:21:14 -0500

    se realiza responsive de encabezado

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,34 +1,77 @@
-import { BottomNavigation, BottomNavigationAction, Grid, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
+import { BottomNavigation, BottomNavigationAction, Grid, Icon, IconButton, Menu, MenuItem, Typography } from '@material-ui/core'
 import { useStyles } from './styles'
 
 const Header = () => {
     const classes = useStyles()
-    return (
-            <Grid container className={classes.root}>
-                <img src="/logo.png" width="150px" className={classes.img} />
-                <Grid item xs={2} className={classes.container}>
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
 
-                </Grid>
-                <Grid item xs={4} className={`${classes.container} ${classes.centerVertical}`}>
-                    <Typography variant="subtitle2" component="h2">
-                        COLECTIVO DE ABOGADOS
-                    </Typography>
-                    <Typography variant="h5" component="h2">
-                        GARCIA & ASOCIADOS
-                    </Typography>
-                </Grid>
-                <Grid item xs={6}>
-                    <BottomNavigation
-                        showLabels
-                        className={classes.container}
-                    >
-                        <BottomNavigationAction classes={{ label: classes.label }} label="INICIO" />
-                        <BottomNavigationAction classes={{ label: classes.label }} label="QUIENES SOMOS" />
-                        <BottomNavigationAction classes={{ label: classes.label }} label="SERVICIOS" />
-                        <BottomNavigationAction classes={{ label: classes.label }} label="PROFESIONALES" />
-                    </BottomNavigation>
-                </Grid>
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <Grid container className={classes.root}>
+            <img src="/logo.png" className={classes.img} />
+            <Grid item xs={3} lg={2} className={classes.container}>
+
             </Grid>
+            <Grid item xs={7} lg={4} className={`${classes.container} ${classes.centerVertical}`}>
+                <Typography variant="subtitle2" component="h2" className={classes.hiddenMovil}>
+                    COLECTIVO DE ABOGADOS
+                </Typography>
+                <Typography variant="h5" component="h2" className={classes.hiddenMovil}>
+                    GARCIA & ASOCIADOS
+                </Typography>
+                <Typography variant="caption" component="text" className={classes.hiddenDesktop}>
+                    COLECTIVO DE ABOGADOS
+                </Typography>
+                <Typography variant="caption" component="text" className={classes.hiddenDesktop}>
+                    GARCIA & ASOCIADOS
+                </Typography>
+            </Grid>
+            <Grid item xs={2} lg={6} className={classes.container}>
+                <IconButton
+                    className={`${classes.container} ${classes.hiddenDesktop}`}
+                    onClick={handleClick}
+                >
+                    <Icon>{open ? 'close' : 'reorder'}</Icon>
+                </IconButton>
+                <BottomNavigation
+                    showLabels
+                    className={`${classes.container} ${classes.hiddenMovil}`}
+                >
+                    <BottomNavigationAction classes={{ label: classes.label }} label="INICIO" />
+                    <BottomNavigationAction classes={{ label: classes.label }} label="QUIENES SOMOS" />
+                    <BottomNavigationAction classes={{ label: classes.label }} label="SERVICIOS" />
+                    <BottomNavigationAction classes={{ label: classes.label }} label="PROFESIONALES" />
+                </BottomNavigation>
+            </Grid>
+            <Menu
+                anchorEl={anchorEl}
+                open={open}
+                className={`${classes.paper} ${classes.hiddenDesktop}`}
+                onClose={handleClose}
+                anchorOrigin={{ horizontal: 100, vertical: 100 }}
+            >
+                <MenuItem className={classes.text}>
+                    INICIO
+                </MenuItem>
+                <MenuItem className={classes.text}>
+                    QUIENES SOMOS
+                </MenuItem>
+                <MenuItem className={classes.text}>
+                    SERVICIOS
+                </MenuItem>
+                <MenuItem className={classes.text}>
+                    PROFESIONALES
+                </MenuItem>
+            </Menu>
+        </Grid >
     )
 }
 
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -9,11 +9,17 @@ export const useStyles = makeStyles((theme) => ({
         height: 150,
         top: 30,
         zIndex: 99,
+        [theme.breakpoints.down("md")]: {
+            height: 50,
+        }
     },
     container: {
         backgroundColor: theme.palette.primary.main,
         color: 'white',
         height: 80,
+        [theme.breakpoints.down("md")]: {
+            height: 40,
+        }
     },
     centerVertical: {
         display: 'flex',
@@ -23,10 +29,40 @@ export const useStyles = makeStyles((theme) => ({
     img: {
         position: 'absolute',
         left: 50,
+        width: "150px",
+        [theme.breakpoints.down("md")]: {
+            width: "50px",
+            left: 30,
+        }
     },
     label: {
         "&.MuiBottomNavigationAction-label": {
             fontSize: '1rem',
         }
     },
+    hiddenMovil: {
+        [theme.breakpoints.down("md")]: {
+            display: 'none',
+        }
+    },
+    hiddenDesktop: {
+        display: 'none',
+        [theme.breakpoints.down("md")]: {
+            display: 'flex',
+        }
+    },
+    paper: {
+        marginTop: 55,
+        "& .MuiMenu-paper": {
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.primary.contrastText,
+            borderBottomLeftRadius: 40,
+        },
+
+    },
+    text: {
+        display: 'flex',
+        justifyContent: 'center ',
+        //borderBottom: 'solid 1px white'
+    }
 }))
