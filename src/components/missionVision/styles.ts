1127aad78f4979106a6f779701c9160a3bfa5f91
Jose Luis Rodriguez <onlin1992@gmail.com>
2021-11-25 11:31:08 -0500

    revisado responsive

--- src/components/missionVision/styles.ts
+++ src/components/missionVision/styles.ts
@@ -1,47 +1,48 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core'
 
-export const useStyles = makeStyles((theme) => ({
-    container: {
-        padding: '30px 10px',
-        [theme.breakpoints.up("md")]: {
-            padding: '30px 10px',
-        }
-    },
-    gridItem: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingRight: 30,
-        paddingLeft: 30,
-    },
-    card: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        minHeight: 350,
-    },
-    text: {
-        color: theme.palette.primary.main,
-        [theme.breakpoints.up("md")]: {
-            fontSize: 20,
-        }
-    },
-    paragraph: {
-        fontSize: 12,
-        textAlign: 'justify',
-        [theme.breakpoints.up("md")]: {
-            fontSize: 12,
-        }
-    },
-    icon: {
-        fontSize: 50,
-        [theme.breakpoints.up("md")]: {
-            fontSize: 20,
-        }
-    },
-    cover: {
-        width: 200,
-        height: 350,
+export const useStyles = makeStyles(theme => ({
+  container: {
+    padding: '30px 10px',
+    [theme.breakpoints.up('md')]: {
+      padding: '30px 10px'
     }
-}
-));
+  },
+  gridItem: {
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingRight: 30,
+      paddingLeft: 30
+    }
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    minHeight: 350
+  },
+  text: {
+    color: theme.palette.primary.main,
+    [theme.breakpoints.up('md')]: {
+      fontSize: 20
+    }
+  },
+  paragraph: {
+    fontSize: 12,
+    textAlign: 'justify',
+    [theme.breakpoints.up('md')]: {
+      fontSize: 12
+    }
+  },
+  icon: {
+    fontSize: 50,
+    [theme.breakpoints.up('md')]: {
+      fontSize: 20
+    }
+  },
+  cover: {
+    width: 200,
+    height: 350
+  }
+}))
